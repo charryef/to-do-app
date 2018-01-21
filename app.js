0eede716f1ea64265c8e2f17eab495552fb602ff
Charry Fuller <charryef@gmail.com>
2018-01-21 07:20:01 -0800

    Assignment 10 completed

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 function onReady() {
-  const addToDoForm = document.getElementById('addToDoForm');
+  const addToDoForm = document.getElementById('submitButton');
   const newToDoText = document.getElementById('newToDoText');
   const toDoList = document.getElementById('toDoList');
+  const deleteToDoText = document.getElementById('deleteToDoText');
 
-  addToDoForm.addEventListener();
-  addToDoForm.addEventListener('submit', event => {
+  addToDoForm.addEventListener('click', event => {
+    console.log("hello");
     event.preventDefault();
 
     // get the text
@@ -12,7 +13,15 @@ function onReady() {
 
     // create a new li
     let newLi = document.createElement('li');
-
+    //create a second button input
+    let removeButton = document.createElement('input');
+    //set the input's type to button
+    removeButton.type = "button";
+    //name the button
+    removeButton.value = "Delete To-Do";
+    removeButton.onclick = function(e) {
+      deleteTodo(e);
+    }
     // create a new input
     let checkbox = document.createElement('input');
 
@@ -20,18 +29,26 @@ function onReady() {
     checkbox.type = "checkbox";
 
     // set the title
-    newLi=textContent = title;
+    newLi.textContent = title;
 
     // attach the checkbox to the li
     newLi.appendChild(checkbox);
 
     // attach the li to the ul
     toDoList.appendChild(newLi);
-
+    //append deleteButton to the li
+    newLi.appendChild(removeButton);
     // empty the input
     newToDoText.value = '';
   });
+
+}
+function deleteTodo(e){
+  console.log(e)
+    let li = e.target.parentNode;
+      let ul = li.parentNode;
+        ul.removeChild(li);
 }
 window.onload = function () {
-
+  onReady();
 };
